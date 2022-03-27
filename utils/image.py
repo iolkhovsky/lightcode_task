a05e33d7760f57967288d00d09fece362e3da0ba
Igor Olkhovskiy <i.v.olkhovsky@gmail.com>
2022-03-27 14:00:15 +0300

    Refactor segment_cuboid_main_face

--- utils/image.py
+++ utils/image.py
@@ -27,15 +27,16 @@ def detect_circles(cv_img: np.ndarray) -> np.ndarray:
 
 def segment_cuboid_main_face(cv_img):
     gray = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
-    hist = cv2.calcHist([gray], [0], None, [256], [0,256])
-    face_brightness = np.argmax(hist.flatten())
-    _, thresh1 = cv2.threshold(gray, int(2.1 * face_brightness), 1, cv2.THRESH_BINARY)
-    mask = thresh1.astype(np.uint8) * 255
-    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+    hist = cv2.calcHist([gray], [0], None, [16], [0, 256])
+    histogram_sorted = sorted(enumerate(hist), key=lambda x: x[1], reverse=True)
+    main_face_brightness = histogram_sorted[1][0] * 16
+    _, binary_img = cv2.threshold(gray, int(main_face_brightness), 1, cv2.THRESH_BINARY)
+    binary_img = binary_img.astype(np.uint8) * 255
+    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(binary_img, 4, cv2.CV_32S)
     assert num_labels > 0
     idx, stat = max(enumerate(stats[1:]), key=lambda x: x[1][cv2.CC_STAT_AREA])
-    final_mask = labels == (idx + 1)
-    return (final_mask * 255).astype(np.uint8), stat[cv2.CC_STAT_AREA]
+    mask = labels == (idx + 1)
+    return (mask * 255).astype(np.uint8), stat[cv2.CC_STAT_AREA]
 
 
 def detect_cuboid_face(cv_img):
